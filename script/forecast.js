72f6f960a1ff676f11069ae960e2d7f59dd5ff95
Knight <mayanks365@gmail.com>
2021-01-16 18:45:32 +0530

    updating files

--- script/forecast.js
+++ script/forecast.js
@@ -1,9 +1,20 @@
-const key = 't9legpG9USnQv7mzkPz6kTC1npNtdhWp';
+let s = '';
+let key = [];
+db.collection('secretKey').get()
+    .then( (data) => {
+        s = data.docs[0].data().k
+        }
+        // data.docs.forEach( doc => {
+        //     key.push(doc.data())
+        //     s = key[0].k
+        //     console.log(s)
+        // })
+)
 
 
 const getWeather = async (locationId) => {
     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${locationId}?apikey=${key}`;
+    const query = `${locationId}?apikey=${s}`;
 
     const response = await fetch( base + query );
     const data = await response.json();
@@ -13,7 +24,7 @@ const getWeather = async (locationId) => {
 
 const getCity = async (city) =>{
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const query = `?apikey=${key}&q=${city}`;
+    const query = `?apikey=${s}&q=${city}`;
 
     const reponse = await fetch( base + query );
     const data = await reponse.json();
